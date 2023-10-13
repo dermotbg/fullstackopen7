dcacb7436e49336108ba580c979943e3267edaac
Dermot Bateman <dermotbateman@gmail.com>
2023-10-13 15:08:56 +0300

    bugfix: infinite GET req loop / OCD Add: resetValues on submit

--- ultimate-hooks/src/App.jsx
+++ ultimate-hooks/src/App.jsx
@@ -8,25 +8,28 @@ const useField = (type) => {
     setValue(event.target.value)
   }
 
+  const resetValues = () => setValue('')
+
   return {
     type,
     value,
-    onChange
+    onChange,
+    resetValues
   }
 }
 
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
-  
+
   useEffect(() => {
   axios.get(baseUrl)
     .then(response => setResources(response.data))
     .catch(error => console.error(error))
-    },[resources, baseUrl])
+  },[baseUrl])
 
   const create = (resource) => {
     axios.post(baseUrl, resource)
-      .then(response => console.log(response.data))
+      .then(response => setResources([...resources, response.data]))
       .catch(error => console.error(error))
   }
 
@@ -54,28 +57,35 @@ const App = () => {
       timestamp: new Date().toISOString(), 
       important: false 
     })
+    content.resetValues()
   }
- 
+  
   const handlePersonSubmit = (event) => {
     event.preventDefault()
     personService.create({ 
       name: name.value, 
-      number: number.value})
+      number: number.value
+    })
+    name.resetValues()
+    number.resetValues()
   }
+  const {resetValues: _1, ...contentProps} = content
+  const {resetValues: _2, ...nameProps} = name
+  const {resetValues: _3, ...numberProps} = number
 
   return (
     <div>
       <h2>notes</h2>
       <form onSubmit={handleNoteSubmit}>
-        <input {...content} />
+        <input {...contentProps} />
         <button>create</button>
       </form>
       {notes.map(n => <p key={n.id}>{n.content}</p>)}
 
       <h2>persons</h2>
       <form onSubmit={handlePersonSubmit}>
-        name <input {...name} /> <br/>
-        number <input {...number} />
+        name <input {...nameProps} /> <br/>
+        number <input {...numberProps} />
         <button>create</button>
       </form>
       {persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
